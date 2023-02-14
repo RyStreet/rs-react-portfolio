86b9e6512ba2e3ddc69f876b32bf93f1a073d6c9
Ryan Street <ryanstreet122@gmail.com>
2023-02-14 15:04:12 -0600

    navbar spacing properly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
     return(
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" id="name" href="#">Ryan Street</a>
-          <div id="links" class="navbar-nav">
+          <div id="links" class="navbar-nav col-3 mr-2">
             <a class="nav-item nav-link" href="">About <span class="sr-only"></span></a>
             <a class="nav-item nav-link" href="">Projects</a>
             <a class="nav-item nav-link" href="">Contact</a>
