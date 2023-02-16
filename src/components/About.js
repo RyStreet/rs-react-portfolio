f92e8274016623e4f5f85d1b429c61f4d8d480bd
Ryan Street <ryanstreet122@gmail.com>
2023-02-16 14:09:40 -0600

    spacing and formatting

--- src/components/About.js
+++ src/components/About.js
@@ -7,17 +7,22 @@ function About() {
   return (
 <div id="about" className="container mb-2 mt-3">
     <div className="row">
-        <h2 className="" >About</h2>
+        <h2 className="col-4" >About</h2>
 
         <div className="container">
             <div className="row justify-content-center">
-                <p className="col-4">
+                <p className="col-md">
                     Hi i'm Ryan, a young professional with experience in content marketing,
                     writing, and web development. Check out some of my projects, apps and
                     writing samples below. Interested in reaching out? Don't hesitate to
                     contact me using the information below. Thanks for visiting my site!
                 </p>
-                <img id="ryanPhoto" className=" img-thumbnail" src={headshot}></img>
+                
+            </div>
+            <div className="container"> 
+            <div className="row justify-content-center">
+            <img id="ryanPhoto" className="col-4 img-thumbnail" src={headshot}></img>
+            </div>
             </div>
         </div>
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,6 +25,7 @@ export default function Projects(){
                     <h5 className="card-title">Munchr</h5>
                     <p className="card-text">A food and drink pairing app</p>
                 </div>
+                
             </div>
             </a>
         </div>
