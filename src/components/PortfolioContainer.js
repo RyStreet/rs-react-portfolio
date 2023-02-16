6cf41ced949596337c356a810395b5c3706411ba
Ryan Street <ryanstreet122@gmail.com>
2023-02-16 15:08:05 -0600

    portfolio container complete

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,12 +1,32 @@
 import React from "react";
 
-
+import Navbar from "./Navbar"
 import About from "./About";
 import Contact from "./Contact";
-import Navbar from "./Navbar";
 import Projects from "./Projects";
 import Resume from "./Resume";
 
 export default function PortfolioContainer () {
-    
+    const [currentPage, setCurrentPage] = useState("About")
+
+    const renderPage = () => {
+        if (currentPage === "About"){
+            return <About/>
+        }
+        if(currentPage === "Contact"){
+            return <Contact/>
+        }
+        if(currentPage === "Projects") {
+            return <Projects/>
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page)
+
+    return(
+        <div>
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
+            {renderPage()}
+        </div>
+    )
 }
