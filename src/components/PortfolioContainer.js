ab400f69640059a03e97d566f9966370a75452ee
Ryan Street <ryanstreet122@gmail.com>
2023-02-16 15:03:27 -0600

    creating container for content section

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+import About from "./About";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Resume from "./Resume";
+
+export default function PortfolioContainer () {
+    
+}
