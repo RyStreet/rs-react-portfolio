956865ec16531d3b7e18def9b7849cf41dfeda0f
Ryan Street <ryanstreet122@gmail.com>
2023-02-16 12:36:19 -0600

    added photo, centered about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,26 @@
 import React from "react";
+import headshot from "../images/headshot.jpg"
+import "../styles/about.css"
+
 
 function About() {
   return (
 <div className="container mb-2 mt-3">
     <div className="row">
         <h2 className="" >About</h2>
-            <div className="">
-                <p>
+
+        <div className="container">
+            <div className="row justify-content-center">
+                <p className="col-4">
                     Hi i'm Ryan, a young professional with experience in content marketing,
                     writing, and web development. Check out some of my projects, apps and
                     writing samples below. Interested in reaching out? Don't hesitate to
                     contact me using the information below. Thanks for visiting my site!
                 </p>
+                <img id="ryanPhoto" className=" img-thumbnail" src={headshot}></img>
             </div>
+        </div>
+
     </div>
 </div>
   );
